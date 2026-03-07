21b7adeda5877a09decaa41c108e312a9964467a
paygoline <paygoofficial1@gmail.com>
2026-03-07 10:03:13 +0100

    Update BuyPayId.tsx

--- src/components/BuyPayId.tsx
+++ src/components/BuyPayId.tsx
@@ -161,14 +161,14 @@ const BuyPayId = ({
 
                 <div className="flex items-center justify-between py-4 border-b border-gray-200">
                   <span className="text-gray-700 font-medium">Bank Name</span>
-                  <span className="text-xl font-bold text-gray-900">VFD MFB</span>
+                  <span className="text-xl font-bold text-gray-900">MOMO PSB</span>
                 </div>
 
                 <div className="flex items-center justify-between py-4 border-b border-gray-200">
                   <span className="text-gray-700 font-medium">Account Number</span>
                   <div className="flex items-center space-x-3">
-                    <span className="text-xl font-bold text-gray-900">1043288865</span>
-                    <Button onClick={() => handleCopy('1043288865')} className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 text-sm rounded-md">
+                    <span className="text-xl font-bold text-gray-900">9165902241</span>
+                    <Button onClick={() => handleCopy('9165902241')} className="bg-blue-500 hover:bg-blue-600 text-white px-3 py-1 text-sm rounded-md">
                       Copy
                     </Button>
                   </div>
@@ -176,7 +176,7 @@ const BuyPayId = ({
 
                 <div className="flex items-center justify-between py-4 border-b border-gray-200">
                   <span className="text-gray-700 font-medium">Account Name</span>
-                  <span className="text-xl font-bold text-gray-900">NANBOL JOSHUA DAVID</span>
+                  <span className="text-xl font-bold text-gray-900">AMOS NIMMYEL</span>
                 </div>
               </div>
 
